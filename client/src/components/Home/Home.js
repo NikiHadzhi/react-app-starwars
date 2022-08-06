86e0e6e298b8975986af311a1214d67b0a8f0751
NikiHadzhi <niki.hadzhi@gmail.com>
2022-08-06 13:37:05 +0300

    Add new homepage logo

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,7 +3,7 @@ import style from './Home.module.css'
 export const Home = () => {
     return (
         <div className={style.homeContainer}>
-            <img className={style.imageBox}src="https://www.citypng.com/public/uploads/small/51608494295fw9yx7dxlamdszf7bwc1l4mhydbzunv63ynjobjeorv80qhin2fwbblkwf6rztbkga5gmzisv0vrcdoahbwwohereq4rsa1bref2.png" alt="Loading..." />
+            <img className={style.imageBox}src="../../star-wars-logo.png" alt="Loading..." />
         </div>
     )
 }   
