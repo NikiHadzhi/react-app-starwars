41a986b80ec2aa1609ed5e94d8e1f5d13fcf6ffc
NikiHadzhi <niki.hadzhi@gmail.com>
2022-08-06 14:01:29 +0300

    Fix details button on catalog page
    Center the text in the button

--- client/src/components/Catalog/Item/Character.js
+++ client/src/components/Catalog/Item/Character.js
@@ -1,23 +1,20 @@
 import style from './Character.module.css'
 import { NavLink } from 'react-router-dom';
 
-import { useContext } from 'react';
-import { AuthContext } from '../../../context/AuthContext';
 
 export const Character = ({
     char
 }
 ) => {
-    const { user } = useContext(AuthContext);
 
     return (
         <li className={style.element}>
             <div className={style.container}>
                 <h2>{char.name}</h2>
                 <img className={style.image} src={char.imgUrl} alt="Loading..." />
-                {user.email && (
-                    <NavLink className={style.link} to={`/catalog/details/${char._id}`}>Details</NavLink>
-                )}
+                <div className={style.detailsBtn}>
+                    <NavLink className={style.link}to={`/catalog/details/${char._id}`}>Details</NavLink>
+                </div>
             </div>
         </li>
     )
